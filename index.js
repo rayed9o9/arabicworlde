93617809c864b7909a18190999f1c2859254a55c
Rayed9o9 <rayed.8@gmail.com>
2023-04-11 23:01:55 +0300

    :D

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
-// create an express app
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
 
-// use the express-static middleware
-app.use(express.static("public"))
-
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
-
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.use(express.static(path.join(__dirname, 'public')));
+ 
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+  //__dirname : It will resolve to your project folder.
+});
+ 
+router.get('/about',function(req,res){
+  res.sendFile(path.join(__dirname+'/shareyourword.html'));
+});
+ 
+ 
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+ 
+console.log('Running at Port 3000');
