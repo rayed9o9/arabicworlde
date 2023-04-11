589c62ffa64569855e33cbbde586f9e941e1b42b
Rayed9o9 <rayed.8@gmail.com>
2023-04-12 02:05:08 +0300

    d

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const router = express.Router();
+const express = require('express')
+const app = express()
+const port = 3000 || process.env.port
 
-app.use(express.static(path.join(__dirname, 'public')));
- 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/index.html'));
-  //__dirname : It will resolve to your project folder.
-});
- 
-router.get('/about',function(req,res){
-  res.sendFile(path.join(__dirname+'/shareyourword.html'));
-});
- 
- 
-//add the router
-app.use('/', router);
-app.listen(process.env.port || 3000);
- 
-console.log('Running at Port 3000');
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
